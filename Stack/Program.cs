20e8050ad812977033bc62bccec9adbcd5d89595
Khud0 <pavement.cafe.37@gmail.com>
2020-06-04 13:45:37 +0200

    Initial Commit

---
+++ Stack/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+
+            stack.Push(1);
+            stack.Push("Doodad");
+            stack.Push(new Point());
+            stack.PrintStatus();
+
+            Console.WriteLine($"Popped object: {stack.Pop()}"); 
+            Console.WriteLine($"Popped object: {stack.Pop()}");         
+            stack.PrintStatus();
+            
+            stack.Clear();
+            stack.PrintStatus();
+        }
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+
+namespace Stack
+{
+    class Stack
+    {
+        private ArrayList list = new ArrayList();
+
+        public void Push(object obj)
+        {
+            if (obj == null) { throw new InvalidOperationException("You can't add null to the stack!"); }
+
+            list.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if (list.Count == 0) { throw new InvalidOperationException("Stack is empty!"); }
+
+            int lastIndex = list.Count - 1;
+            object returnObject = list[lastIndex];
+            list.RemoveAt(lastIndex);
+
+            return returnObject;
+        }
+
+        public void Clear()
+        {
+            if (list.Count > 0) 
+            {
+                Console.WriteLine("Stack was cleared.");
+                list = new ArrayList();
+            }
+        }
+
+        public void PrintStatus()
+        {
+            int count = list.Count;
+
+            Console.WriteLine($"\nNumber of objects on Stack: {count}");
+            for (int i=0; i<count; i++)
+            {
+                Console.WriteLine($"#{i+1}: {list[i].ToString()}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
